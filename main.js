d4061529fb65fc810a7d916304bab3d2f98ca6d2
schneckifyyy <adamogris@outlook.at>
2024-11-19 11:23:12 +0100

    Add Random Location Button and Display for User Story 3

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
-'use strict'
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(data => {
-        const listElement = document.getElementById('list');
-        const jsonString = JSON.stringify(data, null, 2);
-        const preElement = document.createElement('pre');
-        preElement.textContent = jsonString;
-        listElement.appendChild(preElement);
-    });
+'use strict';
 
-/*
-fetch('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TRAUMHOCHZEITOGD&srsName=EPSG:4326&outputFormat=json')
-    .then(response => response.json())
-    .then(data => {
-        const listElement = document.getElementById('WienList');
-        const jsonString = JSON.stringify(data, null, 2);
-        const preElement = document.createElement('pre');
-        preElement.textContent = jsonString;
-        listElement.appendChild(preElement);
-    });
+// Define Location class
+class Location {
+    constructor(ObjectID, ID, Location, Bezirk, Adresse, TelefonNummer, Email, Weblink) {
+        this.ObjectID = ObjectID;
+        this.ID = ID;
+        this.Location = Location;
+        this.Bezirk = Bezirk;
+        this.Adresse = Adresse;
+        this.TelefonNummer = TelefonNummer;
+        this.Email = Email;
+        this.Weblink = Weblink;
+    }
+}
 
- */
-let locations= [];
+// Fetch data function
+async function fetchData() {
+    const response = await fetch('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TRAUMHOCHZEITOGD&srsName=EPSG:4326&outputFormat=json');
+    const data = await response.json();
+    return data.features.map(feature => feature.properties); // Extract `properties`
+}
 
-let data = fetch('https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TRAUMHOCHZEITOGD&srsName=EPSG:4326&outputFormat=json')
-    .then(response => response.json());
+// DOM elements
+let locationButton = document.getElementById("LocationButton");
+let locationDiv = document.getElementById("location");
 
-console.log(data);
+// Function to display a random location
+async function printRndLocation() {
+    const locations = await fetchData(); // Wait for the data to be fetched
+    let rnd = Math.floor(Math.random() * locations.length); // Get a random index
+    let location = locations[rnd];
+
+    // Create a new Location object using named properties
+    let loc = new Location(
+        location.OBJECTID,
+        location.ID,
+        location.LOCATION,
+        location.BEZIRK,
+        location.ADRESSE,
+        location.TELEFONNUMMER,
+        location.EMAIL,
+        location.WEBLINK1
+    );
+
+    // Display the location details
+    locationDiv.innerHTML = `
+        <strong>${loc.Location}</strong><br>
+        Address: ${loc.Adresse}<br>
+        Phone: ${loc.TelefonNummer}<br>
+        Email: ${loc.Email}<br>
+        Website: <a href="${loc.Weblink}" target="_blank">${loc.Weblink}</a>
+    `;
+}
+
+
+locationButton.addEventListener("click", printRndLocation);
+
